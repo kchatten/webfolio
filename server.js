3472d6e59c2eb0763b554210daf7c5bae4ff9b6a
kchatten <kyalchatten@gmail.com>
2024-02-15 17:28:15 -0500

    Webfolio
    Created a repository, linked it to Plesk.
    Created basic server with a homepage that displays under construction to test the production pipeline.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs")
+
+app.use(express.static(__dirname + `/public`))
+
+app.get("/", (req, res) => {
+    res.render(`homepage`); 
+})
+
+
+app.listen(port, () => {
+        console.log(`Server is running on ${port}`);
+})
+
