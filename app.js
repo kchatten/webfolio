9add7681f0d4cf609a9073ca8814b570e3910fa7
kchatten <kyalchatten@gmail.com>
2024-02-17 15:14:44 -0500

    conn test
    index.ejs must exist in views with file extension defined.

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const port = 3000;
 
 app.set("view engine", "ejs")
 
-app.use(express.static(__dirname + `/public`))
-
 app.get("/", (req, res) => {
-    res.render(`index`); 
+    res.render(`index.ejs`); 
 })
-
 
 app.listen(port, () => {
         console.log(`Server is running on ${port}`);
