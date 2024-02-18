6ff465dc3c410b01d1953883e731642b2c52c8d7
kchatten <kyalchatten@gmail.com>
2024-02-18 08:47:43 -0500

    learning

--- app.js
+++ app.js
@@ -1,61 +1,32 @@
 // IMPORT MODULES
 const express = require(`express`);
-const mariadb = require(`mariadb`);
-
-const authRoutes = require(`./routes/authRoutes`);
-
 
 // CREATE EXPRESS APPLICATION
 const app = express();
 
-// MIDDLEWARE
+// DEFINE PORT
+const port = 3000;
 
+// MIDDLEWARE
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
-// MOUNTS
-
-app.use(`/auth`, authRoutes); // Mount authRoutes module to /auth path
-
-// MARIADB POOL
-
-const pool = mariadb.createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'Owner',
-    password: 'aMf5@r705',
-    database: 'gameproj'
-  });
-
 // APP SETS
-
 app.set("view engine", "ejs")
 
-app.set(`views`, `public/views`)
+app.set(`views`, `public`)
 
 // APP GETS
-
 app.get("/", (req, res) => {
     res.render(`index.ejs`); 
 })
 
-app.get("/user", async (req,res) =>{
-    try{
-    res.render(`user.ejs`);
-    } catch (error){
-        res.status(500).send('Internal Service Error');
-    }
-})
-
-app.get("/auth/register", (req, res) =>{
-    app.render("index.ejs");
+app.get("page2", (req, res) => {
+    res.render(`.ejs`); 
 })
 
 
-// DEFINE PORT
-
-const port = 3000;
-
+// CLEAN UP
 app.listen(port, () => {
         console.log(`Server is running on ${port}`);
 })
