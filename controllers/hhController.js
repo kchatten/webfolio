f179c7dfe125edf8f5376035ce5b59bd0ca54a87
kchatten <kyalchatten@gmail.com>
2024-03-01 15:56:55 -0500

    abc

--- controllers/hhController.js
+++ controllers/hhController.js
@@ -1,7 +1,9 @@
+// This controller works directly with hhRouter to direct traffic and handle data. 
+
 exports.homepage = (req, res) => {
     res.render(`habitheroes/index.ejs`);
 }
 
 exports.register = (req, res) => {
-    res.send("TODO: Registration")
+    res.render('habitheroes/register.ejs')
 }
