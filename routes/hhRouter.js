09093075f2a97f74cff122b1e27f4e6a542765ad
kchatten <kyalchatten@gmail.com>
2024-03-02 01:43:01 -0500

    protopage

--- routes/hhRouter.js
+++ routes/hhRouter.js
@@ -1,11 +1,13 @@
+// This works in conjunction with hhController to redirect traffic and handle data related to Habit Heroes.
+
 const express = require(`express`);
 const router = express.Router();
 const hhController = require('../controllers/hhController.js')
 
 
-
 router.get('/', hhController.homepage);
-router.get('/register', hhController.register);
+router.get('/settings', hhController.settings)
+router.post('/register', hhController.register);
 
 
 module.exports = router;
