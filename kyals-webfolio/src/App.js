3aaabe78f422e2b8fa0d43032cab81b12b918cd6
kchatten <kyalchatten@gmail.com>
2024-03-15 13:40:27 -0400

    React Refactor

---
+++ kyals-webfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+
+    // Temporary header
+    <div>     
+        <div id="header">
+        <div className='screw-container'><div className="screw-outer"><p className="screw-inner">+</p></div><div className="screw-outer"><p className="screw-inner">+</p></div></div>
+        Thank you for your interest in my webfolio! I'm currently preoccupied with my studies but I am working hard to get this updated! - kChat
+        <div className='screw-container'><div className="screw-outer"><p className="screw-inner">+</p></div><div className="screw-outer"><p className="screw-inner">+</p></div></div>
+        </div>
+    </div>
+  )
+}
+
+export default App;
