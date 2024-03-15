034cf4bb0a13b6d2c8808bf9002a78204fdf7c36
kchatten <kyalchatten@gmail.com>
2024-03-15 17:48:23 -0400

    Update vite.config.js

--- kyals-webfolio/vite.config.js
+++ kyals-webfolio/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Specify the output format and MIME type for JavaScript files
+    rollupOptions: {
+      output: {
+        format: 'es', // Specify the output format (ES module)
+        mimeTypes: {
+          '.js': 'application/javascript', // Set the MIME type for .js files
+        },
+      },
+    },
+  },
+});
